66343a676143f7546e02b162af35e45351fb6937
nahyeon-an <skgus923@gmail.com>
2022-05-31 23:50:20 +0900

    vuetify: gnb dropdown

---
+++ vuetify-todo/src/components/DropdownNavbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    class="dropdown-navigation"
+    :style="{ top: this.headerHeight + 'px' }"
+    v-if="this.isActive"
+  >
+    <h2>CARD1</h2>
+    <h2>CARD2</h2>
+    <h2>CARD3</h2>
+    <h2>CARD4</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DropdownNavbar",
+  data() {
+    return {
+      headerHeight: 0,
+    };
+  },
+  props: {
+    isActive: Boolean,
+  },
+  mounted() {
+    window.addEventListener("resize", this.handleResize);
+    this.headerHeight = document.getElementsByTagName("header")[0].clientHeight;
+  },
+  methods: {
+    handleResize(event) {
+      this.headerHeight =
+        document.getElementsByTagName("header")[0].clientHeight;
+      console.log(event);
+    },
+  },
+  watch: {
+    headerHeight() {
+      console.log(this.headerHeight);
+    },
+  },
+};
+</script>
+
+<style>
+div.dropdown-navigation {
+  width: 100%;
+  position: absolute;
+  border: 1px #f5f5f5;
+  background-color: white;
+  overflow: auto;
+  z-index: 10;
+}
+</style>
--- vuetify-todo/src/components/Navbar.vue
+++ vuetify-todo/src/components/Navbar.vue
@@ -5,12 +5,17 @@
         class="grey--text"
         @click="drawer = !drawer"
       ></v-app-bar-nav-icon>
+
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>ninja</span>
       </v-toolbar-title>
 
       <v-spacer></v-spacer>
+
+      <v-btn text color="grey" @click="dropdown = !dropdown">
+        <v-icon>mdi-apps</v-icon>
+      </v-btn>
 
       <v-btn text color="grey">
         <span>Sign Out</span>
@@ -21,19 +26,27 @@
     <v-navigation-drawer v-model="drawer" app class="indigo">
       <p>Test</p>
     </v-navigation-drawer>
+
+    <dropdown-navbar :isActive="dropdown"></dropdown-navbar>
   </nav>
 </template>
 <script>
+import DropdownNavbar from "./DropdownNavbar.vue";
 export default {
+  components: { DropdownNavbar },
   name: "Navbar",
   data() {
     return {
       drawer: false,
+      dropdown: false,
     };
   },
   watch: {
     drawer() {
       console.log(this.drawer);
+    },
+    dropdown() {
+      console.log(`dropdown: ${this.dropdown}`);
     },
   },
 };
